2fefe840f4255aa25ac0966a7dbdd846dbb1fce6
Michael Gargano <Michael.Gargano@jax.org>
2020-06-01 14:03:46 -0400

    Adding in reporting formats

--- seep_hpo/main.py
+++ seep_hpo/main.py
@@ -12,25 +12,30 @@ def cli():
 @cli.command()
 @click.argument('annotation_path', type=click.Path(exists=True))
 @click.argument('query_path', type=click.Path(exists=True))
-def resolve(annotation_path, query_path):
+@click.option('--report', default='tsv', help='Format to report in. One of (json, csv, tsv).'
+                                              'Default: tsv')
+def resolve(annotation_path, query_path, report_format):
     """Command to resolve loinc 2 hpo mappings.
 
     ANNOTATION_PATH is the path to the LOINC HPO Annotation File.
     QUERY_PATH is the path to your file with Loinc Id's, Measures, Negations.
     """
-    click.echo("Parsing annotation files...")
+    click.echo('Parsing annotation files...')
     click.echo(click.format_filename(annotation_path))
     click.echo(click.format_filename(query_path))
     annotations = AnnotationParser.parse_annotation_file_dict(annotation_path)
     queries = QueryFileParser.parse(query_path)
     resolver = AnnotationResolver(annotations)
-    click.echo("Resolving your queries...")
+    click.echo('Resolving your queries...')
     for query in queries:
         result = resolver.resolve(query)
-        print(result)
-    click.echo("Done.")
-    # Then we need to read in the queries. For each query we will call the resolver.
-    # Then write a reporter to report our findings in different formats.
+        if report_format == 'json':
+            print('{{0}:{{1}:{{2}:{3}}}}'.format(query.loinc_id, query.measure, query.negated,
+                                                 result))
+        elif report_format == 'csv':
+            print('{0},{1},{2},{3}'.format(query.loinc_id, query.measure, query.negated, result))
+        else:
+            print('{0}\t{1}\t{2}\t{3}'.format(query.loinc_id, query.measure, query.negated, result))
 
 
 @cli.command()
--- seep_hpo/tests/test_cli.py
+++ seep_hpo/tests/test_cli.py
@@ -9,8 +9,8 @@ class CliTest(unittest.TestCase):
     def test_version_cli(self):
         runner = CliRunner()
         result = runner.invoke(cli, ['version'])
-        assert result.exit_code == 0
-        assert "Version not configured yet." in result.output
+        self.assertEqual(result.exit_code, 0)
+        self.assertIn("Version not configured yet.", result.output)
 
     def test_resolver_cli(self):
         runner = CliRunner()
@@ -19,5 +19,5 @@ class CliTest(unittest.TestCase):
         test_query_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                            'test_query.tsv')
         result = runner.invoke(cli, ['resolve', test_annotaion_file, test_query_file])
-        assert "HP:0011042" in result.output
-        assert "HP:0003362" in result.output
+        self.assertIn("HP:0011042", result.output)
+        self.assertIn("HP:0003362", result.output)
